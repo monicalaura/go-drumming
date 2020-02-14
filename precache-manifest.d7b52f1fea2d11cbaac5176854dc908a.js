8588f3db93fd6d0aee1384877691c9466a855c52
monicalaura <monicalauraburns@yahoo.com>
2020-02-14 13:09:44 +0200

    Updates

---
+++ precache-manifest.d7b52f1fea2d11cbaac5176854dc908a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef361a0b054a7204bdd5e0aadf6378f",
+    "url": "/monicalaura/go-drumming/index.html"
+  },
+  {
+    "revision": "3c70a1d18564a0c6ad38",
+    "url": "/monicalaura/go-drumming/static/css/main.708769f5.chunk.css"
+  },
+  {
+    "revision": "6db1f9eac7744e8d0df0",
+    "url": "/monicalaura/go-drumming/static/js/2.3ac87236.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monicalaura/go-drumming/static/js/2.3ac87236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c70a1d18564a0c6ad38",
+    "url": "/monicalaura/go-drumming/static/js/main.44501e7c.chunk.js"
+  },
+  {
+    "revision": "97842311dd62b9811f6a",
+    "url": "/monicalaura/go-drumming/static/js/runtime-main.618569c8.js"
+  },
+  {
+    "revision": "e72d703feeefd7dcd3e595957e9c3e7d",
+    "url": "/monicalaura/go-drumming/static/media/background.e72d703f.png"
+  }
+]);
